501c9d7275c6d7842945b10dd58d1e94541e60d0
madair <madair@ncsu.edu>
2018-06-11 13:30:44 -0400

    fixed port issues for ElasticBeanstalk

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var mod = require('./testmodule');
 
+var port = process.env.PORT || 3000;
+
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end("Hello World!<br>" + mod.message());
-}).listen(80);
+}).listen(port);
