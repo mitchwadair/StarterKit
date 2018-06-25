e6189800c3976f37d3381aa642c935e329194511
madair <madair@ncsu.edu>
2018-06-25 17:40:48 -0400

    Adds sidebar menu

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
     $("#siteHeader").load("header.html");
     $("#sideMenu").load("menu.html");
+
+    $('#menuToggleButton').on('click', function () {
+        $('#sideMenu').toggleClass('active');
+    });
 });
