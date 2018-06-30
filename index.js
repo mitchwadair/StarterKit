d30d53c9f3e2850094fb5de334b6473df1535596
madair <madair@ncsu.edu>
2018-06-30 16:05:08 -0400

    handles non-200 status on http response

--- index.js
+++ index.js
@@ -30,9 +30,13 @@ var readFromDB = function() {
     );
     xhttp.send();
     var response = xhttp.response;
-    var item = JSON.parse(response).Item;
-    var s = "You've just read from the database!\n";
-    s += "Kit name: " + item.KitName.S + "\n";
-    s += "Kit description: " + item.KitDescription.S;
-    alert(s);
+    if (xhttp.status == 200){
+        var item = JSON.parse(response).Item;
+        var s = "You've just read from the database!\n";
+        s += "Kit name: " + item.KitName.S + "\n";
+        s += "Kit description: " + item.KitDescription.S;
+        alert(s);
+    } else {
+        alert(response);
+    }
 }
