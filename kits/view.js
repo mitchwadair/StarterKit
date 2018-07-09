a79d1c71dfe7012f427c710264f2dc3e2e3aa069
madair <madair@ncsu.edu>
2018-07-09 15:22:24 -0400

    Clicking logo loads home page, fixed some formatting issues with view page

--- kits/view.js
+++ kits/view.js
@@ -34,7 +34,7 @@ var loadKitInfo = function(kitName) {
                 $("#kitDescription").html(kit.KitDescription.S);
                 var s = "<div class='row'>";
                 for (var i = 0; i < kit.Items.L.length; i++) {
-                    s += "</div></div><div class='col-xs-12'><br></div>";
+                    s += "<div class='col-xs-12'><br></div>";
                     s += "<div class='col-xs-12' onclick='window.open(\"" + kit.Items.L[i].L[2].S + "\");' style='cursor: pointer; background: lightgrey'><div class='row'>";
                     s += "<div class='col-sm-6'>";
                     s += "<h4>" + kit.Items.L[i].L[0].S + " <small>$1337</small></h4>"
@@ -42,9 +42,9 @@ var loadKitInfo = function(kitName) {
                     s += "</div>";
                     s += "<div class='col-sm-6'>";
                     s += "<img src='" + kit.Items.L[i].L[3].S + "' class='img-responsive img-rounded' alt='" + kit.Items.L[i].L[0].S + "' style='max-height: 200px; margin: auto'/>";
-                    s += "</div>";
+                    s += "</div></div></div>";
                 }
-                s += "</div>";
+                s += "<div class='col-xs-12'><br></div></div>";
                 $("#kitItems").html(s);
             } else {
                 alert(response);
