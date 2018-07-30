577a18339de509710ffe82717d3f2ae48829bfbf
madair <madair@ncsu.edu>
2018-07-30 16:46:39 -0400

    styling changes to fit new logo better

--- kits/view.js
+++ kits/view.js
@@ -50,10 +50,12 @@ var loadKitInfo = function(kitName) {
                 $("#kitDescription").html(kit.KitDescription.S);
                 var s = "<div class='row'>";
                 for (var i = 0; i < kit.Items.L.length; i++) {
-                    s += "<div class='col-xs-12'><br></div>";
-                    s += "<div class='col-xs-12' onclick='window.open(\"" + kit.Items.L[i].L[2].S + "\");' style='cursor: pointer; background: lightgrey'><div class='row'>";
+                    if (i > 0) {
+                        s += "<div class='col-xs-12'><br></div>";
+                    }
+                    s += "<div class='col-xs-12' onclick='window.open(\"" + kit.Items.L[i].L[2].S + "\");' style='cursor: pointer; background: #00aeef'><div class='row'>";
                     s += "<div class='col-sm-6'>";
-                    s += "<h4>" + kit.Items.L[i].L[0].S + " <small>$1337</small></h4>"
+                    s += "<h4>" + kit.Items.L[i].L[0].S + " <small style='color: #1a1a1a'>$1337</small></h4>"
                     s += "<p>" + kit.Items.L[i].L[1].S + "</p>";
                     s += "</div>";
                     s += "<div class='col-sm-6'>";
